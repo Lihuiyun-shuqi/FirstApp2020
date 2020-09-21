3573c1fd862b8c823cee0aae06606b3634c079a0
lihuiyun <1807228804@qq.com>
2020-09-21 23:31:55 +0800

    CurrencyExchangeRate Converter

--- app/src/main/java/com/swufe/firstapp/MainActivity.java
+++ app/src/main/java/com/swufe/firstapp/MainActivity.java
@@ -2,51 +2,43 @@ package com.swufe.firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
     TextView out;
-    //EditText edit;
+    EditText edit;
+    DecimalFormat df;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*
-        //获取控件 out
-        TextView outobj = findViewById(R.id.textView);
-        outobj.setText("Hello");
-        //Log.i( tag:"loading",  msg:"onCreate: msg......");
-
-        EditText edit = (EditText)findViewById(R.id.editTextTextPersonName);
-        String str = edit.getText().toString();*/
-        out = (TextView) findViewById(R.id.textView6);
-        //edit = (EditText)findViewById(R.id.editTextNumberDecimal);
+        edit = (EditText)findViewById(R.id.editTextNumberDecimal);
+        out = (TextView) findViewById(R.id.textView);
+        df = new DecimalFormat( "0.0000");//设置double类型小数点后位数格式
     }
     public void btna(View v){
-        int score = Integer.parseInt(out.getText().toString());
-        int t = score + 1;
-        String str = String.valueOf(t);
+        double oc = Double.parseDouble(edit.getText().toString());
+        double t = oc * 0.1471;
+        String s = String.valueOf(df.format(t));
+        String str = edit.getText().toString() + " RMB =" + s + " DOLLAR";
         out.setText(str);
     }
     public void btnb(View v){
-        int score = Integer.parseInt(out.getText().toString());
-        int t = score + 2;
-        String str = String.valueOf(t);
+        double oc = Double.parseDouble(edit.getText().toString());
+        double t = oc * 0.125;
+        String s = String.valueOf(df.format(t));
+        String str = edit.getText().toString() + " RMB =" + s + " EURO";
         out.setText(str);
     }
     public void btnc(View v){
-        int score = Integer.parseInt(out.getText().toString());
-        int t = score + 3;
-        String str = String.valueOf(t);
-        out.setText(str);
-    }
-    public void reset(View v){
-        int t = 0;
-        String str = String.valueOf(t);
+        double oc = Double.parseDouble(edit.getText().toString());
+        double t = oc * 171.4571;
+        String s = String.valueOf(df.format(t));
+        String str = edit.getText().toString() + " RMB =" + s + " WON";
         out.setText(str);
     }
 }
