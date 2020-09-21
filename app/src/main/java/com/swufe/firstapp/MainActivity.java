9d23f4d754dd85da16a181bcd48afef174b6f4fd
lihuiyun <1807228804@qq.com>
2020-09-21 21:56:27 +0800

    CourtCounter for two teams（use RelativeLayout and LinearLayout）（not perfect version）

--- app/src/main/java/com/swufe/firstapp/MainActivity.java
+++ app/src/main/java/com/swufe/firstapp/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView out;
+    TextView out1,out2;
     //EditText edit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,30 +23,50 @@ public class MainActivity extends AppCompatActivity {
 
         EditText edit = (EditText)findViewById(R.id.editTextTextPersonName);
         String str = edit.getText().toString();*/
-        out = (TextView) findViewById(R.id.textView6);
+        out1 = (TextView) findViewById(R.id.textView2);
+        out2 = (TextView) findViewById(R.id.textView4);
         //edit = (EditText)findViewById(R.id.editTextNumberDecimal);
     }
     public void btna(View v){
-        int score = Integer.parseInt(out.getText().toString());
-        int t = score + 1;
-        String str = String.valueOf(t);
-        out.setText(str);
-    }
-    public void btnb(View v){
-        int score = Integer.parseInt(out.getText().toString());
-        int t = score + 2;
-        String str = String.valueOf(t);
-        out.setText(str);
-    }
-    public void btnc(View v){
-        int score = Integer.parseInt(out.getText().toString());
+        int score = Integer.parseInt(out1.getText().toString());
         int t = score + 3;
         String str = String.valueOf(t);
-        out.setText(str);
+        out1.setText(str);
+    }
+    public void btnb(View v){
+        int score = Integer.parseInt(out1.getText().toString());
+        int t = score + 2;
+        String str = String.valueOf(t);
+        out1.setText(str);
+    }
+    public void btnc(View v){
+        int score = Integer.parseInt(out1.getText().toString());
+        int t = score + 1;
+        String str = String.valueOf(t);
+        out1.setText(str);
     }
     public void reset(View v){
         int t = 0;
         String str = String.valueOf(t);
-        out.setText(str);
+        out1.setText(str);
+        out2.setText(str);
+    }
+    public void btne(View v){
+        int score = Integer.parseInt(out2.getText().toString());
+        int t = score + 3;
+        String str = String.valueOf(t);
+        out2.setText(str);
+    }
+    public void btnf(View v){
+        int score = Integer.parseInt(out2.getText().toString());
+        int t = score + 2;
+        String str = String.valueOf(t);
+        out2.setText(str);
+    }
+    public void btng(View v){
+        int score = Integer.parseInt(out2.getText().toString());
+        int t = score + 1;
+        String str = String.valueOf(t);
+        out2.setText(str);
     }
 }
