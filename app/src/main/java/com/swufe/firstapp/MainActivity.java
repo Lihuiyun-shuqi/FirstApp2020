e68398c5339c4cfbbaabb1d02079ff103150e833
lihuiyun <1807228804@qq.com>
2020-09-27 23:58:06 +0800

    改进的对两队进行的计分器（使用ConstraintLayout中包含一个LinearLayout（horizontal）和RESET按钮，该LinearLayout（horizontal）中又包含两个LinearLayout（vertical）的方式，使得布局更加合理）

--- app/src/main/java/com/swufe/firstapp/MainActivity.java
+++ app/src/main/java/com/swufe/firstapp/MainActivity.java
@@ -1,52 +1,54 @@
 package com.swufe.firstapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView out;
-    //EditText edit;
+    TextView out1,out2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*
-        //获取控件 out
-        TextView outobj = findViewById(R.id.textView);
-        outobj.setText("Hello");
-        //Log.i( tag:"loading",  msg:"onCreate: msg......");
 
-        EditText edit = (EditText)findViewById(R.id.editTextTextPersonName);
-        String str = edit.getText().toString();*/
-        out = (TextView) findViewById(R.id.textView6);
-        //edit = (EditText)findViewById(R.id.editTextNumberDecimal);
+        out1 = (TextView)findViewById(R.id.textView2);
+        out2 = (TextView)findViewById(R.id.textView4);
     }
-    public void btna(View v){
-        int score = Integer.parseInt(out.getText().toString());
-        int t = score + 1;
-        String str = String.valueOf(t);
-        out.setText(str);
+
+    public void btn(View btn){
+        int score1 = Integer.parseInt(out1.getText().toString());
+        int score2 = Integer.parseInt(out2.getText().toString());
+        if(btn.getId()==R.id.button1){
+            int t = score1 + 3;
+            String str = String.valueOf(t);
+            out1.setText(str);
+        }else if(btn.getId()==R.id.button2){
+            int t = score1 + 2;
+            String str = String.valueOf(t);
+            out1.setText(str);
+        }else if(btn.getId()==R.id.button3){
+            int t = score1 + 1;
+            String str = String.valueOf(t);
+            out1.setText(str);
+        }else if(btn.getId()==R.id.button4){
+            int t = score2 + 3;
+            String str = String.valueOf(t);
+            out2.setText(str);
+        }else if(btn.getId()==R.id.button5){
+            int t = score2 + 2;
+            String str = String.valueOf(t);
+            out2.setText(str);
+        }else if(btn.getId()==R.id.button6){
+            int t = score2 + 1;
+            String str = String.valueOf(t);
+            out2.setText(str);
+        }else if(btn.getId()==R.id.button7){
+            int t = 0;
+            String str = String.valueOf(t);
+            out1.setText(str);
+            out2.setText(str);
+        }
     }
-    public void btnb(View v){
-        int score = Integer.parseInt(out.getText().toString());
-        int t = score + 2;
-        String str = String.valueOf(t);
-        out.setText(str);
-    }
-    public void btnc(View v){
-        int score = Integer.parseInt(out.getText().toString());
-        int t = score + 3;
-        String str = String.valueOf(t);
-        out.setText(str);
-    }
-    public void reset(View v){
-        int t = 0;
-        String str = String.valueOf(t);
-        out.setText(str);
-    }
+
 }
